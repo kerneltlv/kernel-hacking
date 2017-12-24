a1bd314c42a82e2b5296f4736dd7bb284c5730bd
BackSlasher <nitz.raz@gmail.com>
2017-12-24 10:08:13 +0200

    Minor fixes - provision and empty dir (#3)
    * Created empty linux-source directory
    
    Also ignoring vim swaps
    
    * setup_vm.sh is now retry proof

--- setup_vm.sh
+++ setup_vm.sh
@@ -1,16 +1,18 @@
-#!/usr/bin/env bash
+#!/bin/bash
+set -e
 echo "Installing dependencies..."
 sudo apt-get update
 sudo apt-get install -y build-essential ctags libncurses5-dev python-pip
 pip install -r khack/requirements.txt
 
-sudo ln -s `realpath khack/khack` /usr/local/bin/khack
+sudo ln -fs `realpath khack/khack` /usr/local/bin/khack
 
 echo "Extracting kernel source..."
 khack kernel get
 khack kernel config minimal
 chown -R vagrant:vagrant linux-source/
 
+[ ! -e boot-backup ] || rm -r boot-backup
 mkdir boot-backup
 cp /boot/vmlinuz* boot-backup/
 cp /boot/initrd* boot-backup
