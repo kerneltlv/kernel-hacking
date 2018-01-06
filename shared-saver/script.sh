7089bfd6b57686b74d3da05588af4cc2371499f7
nitz <nitz.raz@gmail.com>
2018-01-06 11:18:34 +0200

    Shared saver cleanup, better regex

--- shared-saver/script.sh
+++ shared-saver/script.sh
@@ -1,37 +1,14 @@
 #!/bin/sh
-### BEGIN INIT INFO
-# Provides:
-# Required-Start:    $remote_fs $syslog
-# Required-Stop:     $remote_fs $syslog
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: Saves or loads VBox's shared folders
-# Description:       Saves or loads VBox's shared folders
-### END INIT INFO
-
 set -e
 set -x
 
 # https://raw.githubusercontent.com/fhd/init-script-template/master/template
 
-dir=""
-cmd=""
-user=""
-
-name=`basename $0`
-
 DUMP_FILE=/var/local/shared-saver-dump
-KERNEL_PASSPHRASE=root
-#KERNEL_PASSPHRASE=khack-kexec
-
-LOGFILE="/home/vagrant/shared-saver-log-$(date '+%Y-%m-%d-%H-%M-%S')"
-log() {
-  echo "$@" >>"$LOGFILE"
-}
 
 save_file() {
-  log 'saving mounts'
-  mount | perl -ne 'print "$1\t$2\tvboxsf\tdefaults\t0\t0$/" if /^([\w\/]+) on (.+) type vboxsf \(.+\)$/' | tee "$DUMP_FILE"
+  echo 'saving'
+  mount | perl -ne 'print "$1\t$2\tvboxsf\tdefaults\t0\t0$/" if /^(\S+) on (.+) type vboxsf \(.+\)$/' | tee "$DUMP_FILE"
 }
 
 is_magic() {
@@ -40,14 +17,15 @@ is_magic() {
 
 force_load_file() {
   if [ -f "$DUMP_FILE" ]; then
+    'file found, loading'
     mount -aT "$DUMP_FILE"
   else
-    log 'file not found, skipping'
+    echo 'file not found, skipping'
   fi
 }
 
 load_file() {
-  log 'loading mounts, before checking'
+  echo 'loading mounts, before checking'
   if is_magic; then
    force_load_file 
   fi
@@ -55,20 +33,13 @@ load_file() {
 
 case "$1" in
     start)
-    # TODO do we need to handle /var/lock/subsys? https://unix.stackexchange.com/a/114277
-    # TODO read state if we have the magic passphrase in kernel CLI
     load_file
     ;;
     stop)
-    # TODO dump state to file
     save_file
     ;;
-    status)
-    # We're never running
-    echo "This is useless"
-    ;;
     *)
-    echo "Usage: $0 {start|stop|status}"
+    echo "Usage: $0 {start|stop}"
     exit 1
     ;;
 esac
